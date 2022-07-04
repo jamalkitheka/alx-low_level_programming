804ae4846b83a734dae5e37b80ae53f123ad23b5
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-04 12:28:45 -0700

    4. prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
 {
-if (c == 'e' || c == 'q')
-			;
-else
-putchar (c);
-}
-putchar ('\n');
-return (0);
+	if (c == 'e' || c == 'q')
+		;
+	else
+		putchar (c);
+	}	
+	putchar ('\n');
+	return (0);
 }
