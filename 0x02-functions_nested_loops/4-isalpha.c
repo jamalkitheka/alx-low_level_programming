297bd30859259cd5e48d2a167eccb4c2446bcc40
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-05 13:08:33 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: displays letters in the alphabet
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
