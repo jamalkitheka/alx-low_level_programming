0284477b557d85204bdafff07002a0667deb23ff
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-05 15:00:49 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @j: int type number
+ * @m: int type number
+ * Return: result of addition
+ */
+
+int add(int j, int m)
+{
+	return (j + m);
+}
+
