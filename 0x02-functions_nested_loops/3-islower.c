8354b0b5ddb0ce5ae7af2c1ea5ea46be1f9a2828
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-05 12:29:35 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: displays lowercase letters
+ * Return: 0
+ */
+
+int _islower(int j)
+{
+	if (j >= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
