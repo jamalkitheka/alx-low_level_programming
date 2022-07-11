b7f8b77dc8d1576abd34f2d8e3049de723c0fe1e
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-11 12:03:49 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -function to update the reference of a pointer
+ * @n: pointer to n to be updated
+ * Return: n
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
