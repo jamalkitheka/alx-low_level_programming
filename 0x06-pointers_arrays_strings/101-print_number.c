b02386e3fc17df43a5c2f521ba2c2fee4d74d952
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-15 12:37:36 -0700

    9. Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_number - a function that prints an integer
+ * @n: the integer to print
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if (n > -1)
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
+	}
+}
