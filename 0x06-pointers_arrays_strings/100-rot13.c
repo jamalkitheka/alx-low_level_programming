5812110b061f30f60d8ca1685c60ae852a58c7c5
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-15 17:20:03 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,16 +6,16 @@
  * Return: encoded string
  */
 
-char *rot13(char *s))
+char *rot13(char *s)
 {
-	char *c;
-	char shift;
+	char f, *r = s;
 
-	for (c = s; *c; ++c)
+	while (*s)
 	{
-		shift = 'A' + (*c & 32);
-		if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
-			*c = (*c - shift + 13) % 26 + shift;
+		f = (*s & 32) + 65;
+		if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+			*s = (*s - f + 13) % 26 + f;
+		s++;
 	}
-	return (s);
+	return (r);
 }
