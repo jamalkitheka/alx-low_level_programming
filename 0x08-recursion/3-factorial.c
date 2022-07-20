5cb803c3cc3931e6a0e8f3d19927a3c636610ef1
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-20 13:40:43 -0700

    3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,17 +2,12 @@
 
 /**
  * factorial - function that compute the factorial of a number
- * @n: the integer
- * 
+ * @n: the integer to operate on
  * Return: the factorial of n
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if(n < 2)
-		return (n);
-	else
-		return (n * factorial(n -1));
-	
+	return (n > 1 ? n * factorial(n - 1) : 1);
 }
