9c4ea3de03b285df19bb431bd4f4cc8e8e001949
jamalkitheka <jamalkitheka27@gmail.com>
2022-07-21 06:51:49 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,50 +1,46 @@
 #include "main.h"
 
 /**
- * is_palindrome - function check if a string is palindrom
+ * palind2 - obtain length of a
+ * @a: string
+ * @l: integer to count length
+ * Return: on success 1.
+ */
+
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+
+/**
+ * palind3 - compares string with string reverse
+ * @a: string
+ * @l: length
+ * Return: on success 1
+ */
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+
+/**
+ * is_palindrome - checks if a string is palindrome
  * @s: string
- * Return: 1 if palindrome, 0 if not
+ * Return:0n success 1.
  */
 
 int is_palindrome(char *s)
 {
-	int len;
+	int l;
 
-	len = get_length(s) - 1;
-	return (my_pal(s, --len));
-}
-
-/**
- * get_length - function that gets length of string
- * @s: string
- * Return: length of string
- */
-
-int get_length(char *s)
-{
-	if (*s == '\0')
-		return (1);
-	else
-		return (1 + get_length(++s));
-}
-
-/**
- * my_pal - recursive check of palindrome
- * @s: string
- * @l: length of string
- * Return: 1 if palindrome, 0 if not
- */
-
-int my_pal(char *s, int l)
-{
-	if (*s == *(s + l))
-	{
-		if (l <= 0)
-			return (1);
-		else
-			return (my_pal(++s, l - 2));
-	}
-	else
-		return (0);
+	l = palind2(a, 0);
+	return (palind3(s, 1));
 }
 
