891b19eafc7aeb1b9a157f814a1521b019463c70
jamalkitheka <jamalkitheka27@gmail.com>
2022-08-15 13:09:30 -0700

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,15 +10,12 @@ void free_list(list_t *head)
 {
 	list_t *current, *next;
 
-	if (head != NULL)
+	current = head;
+	while (current != NULL)
 	{
-		current = head;
-		while (current != NULL)
-		{
-			next = current->next;
-			free(current);
-			free(current->str);
-			current = next;
-		}
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
 	}
 }
