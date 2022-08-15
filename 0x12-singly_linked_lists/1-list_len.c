b8ab9c55fcf74cdf0c67d8cb3625c41e260a47ac
jamalkitheka <jamalkitheka27@gmail.com>
2022-08-15 10:13:59 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len -  returns the number of elements in a linked list_t
+ * @h: linked list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len;
+
+	len = 0;
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
