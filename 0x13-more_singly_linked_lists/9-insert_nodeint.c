4dd421856c0220278a4754268130a7eb46daf6cf
jamalkitheka <jamalkitheka27@gmail.com>
2022-08-16 10:49:12 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,12 +3,12 @@
 /**
  * insert_nodeint_at_index - insert a new node at a given position
  * @head: double pointer to head
- * @index: insert node at this index, starting count at 0
+ * @idx: insert node at this index, starting count at 0
  * @n: value to store in node
  *  Return: Address of new node or NULL if failed
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *new;
