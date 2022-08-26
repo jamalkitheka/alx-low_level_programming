6fbddf1a27bc603749f8f52fd4aac763104f6231
jamalkitheka <jamalkitheka27@gmail.com>
2022-08-26 14:35:50 -0700

    1.Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - finds the length of string
- * @str: pointer to the string
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t x;
-
-	for (x = 0; str[x]; x++)
-		;
-	return (x);
-}
-
-/**
  * create_file - creates a file
  * @filename: the file which should be read
  * @text_content: a null terminating string to write to the file
@@ -24,23 +10,20 @@ size_t _strlen(char *str)
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, result;
-	size_t count = _strlen(text_content);
+	int fd, result, x;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-	{
-		write(1, "Opening failed", 15);
 		return (-1);
-	}
-	result = write(fd, text_content, count);
-	if (result <= 0)
+	if (text_content)
 	{
-		return (-1);
+		for (x = 0; text_content[x] != '\0'; x++)
+			;
+		result = write(fd, text_content, x);
+		if (result == -1)
+			return (-1);
 	}
 	close(fd);
 	return (1);
