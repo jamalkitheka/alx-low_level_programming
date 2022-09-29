af0765df4a70cfcb2ff3eb5b68ceacccb222f2d1
jamalkitheka <jamalkitheka27@gmail.com>
2022-09-29 13:41:11 -0700

    4. Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a `dlistint_t` doubly linked list
+ * @head: head pointer
+ * Return: none
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+	while (head)
+	{
+		tmp = head;
+		head = tmp->next;
+		free(tmp);
+	}
+}
